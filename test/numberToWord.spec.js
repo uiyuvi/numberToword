c6a2fe5a0e7e11cd67e094a7cae62eda3aa25c0a
RANGANATHAN Yuvaraj <yuvaraj.ranganathan@bnpparibasfortis.com>
2019-10-11 15:29:17 +0200

    refactor: Move the initialisation before tests

--- test/numberToWord.spec.js
+++ test/numberToWord.spec.js
@@ -2,18 +2,16 @@ const assert = require("chai").assert
 const NumberToWord = require("../src/js/numberToWord");
 
 describe('Numeber to Word', function () {
-    it('Should return "Zero" for number 0', function () {
-        let output;
-        const numberToWord = new NumberToWord();
+    let output;
+    const numberToWord = new NumberToWord();
 
+    it('Should return "Zero" for number 0', function () {
         output = numberToWord.convert(0);
 
         assert.equal('Zero', output);
     });
-    it('should convert to "One" for number 1', () => {
-        let output;
-        const numberToWord = new NumberToWord();
-
+    
+    it('should convert to "One" for number 1', () => {        
         output = numberToWord.convert(1);
         
         assert.equal('One', output);
